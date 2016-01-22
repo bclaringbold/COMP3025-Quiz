f60a3da4ff6d21dadfc76122281f4bb421ab552d
bclaringbold <student@students-mac.student.georgianc.on.ca>
2016-01-22 11:35:37 -0500

    Update

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("The View Did Load")
+    
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let questions: [String] =  ["From what is cognac made?",
+                                "What is 7+7?",
+                                "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+    ++currentQuestionIndex
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
-
-
+    
+    @IBAction func showAnswerClicked(sender: AnyObject) {
+    
+    let answer: String = answers[currentQuestionIndex]
+    answerLabel.text = answer
+    
+    }
+    
 }
 
